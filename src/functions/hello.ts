13e3417fa04040ecf4152d3208ebda5183412859
VictorGabriel1 <victor.mucci1@hotmail.com>
2023-05-15 02:29:26 -0300

    feat: Initial commit

---
+++ src/functions/hello.ts
@@ -0,0 +1,18 @@
+import { APIGatewayEvent } from "aws-lambda";
+
+export const hello = async (event: APIGatewayEvent) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Go Serverless v1.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
